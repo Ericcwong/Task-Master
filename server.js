12871e3127da0642d006df68984dfe37949e890c
Eric Wong <ericwong12@live.com>
2020-04-25 12:35:12 -0600

    added minor changes to server.js added useFindAndModify: false

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
   .then(() => console.log("MongoDB successfully connected"))
   .catch(err => console.log(err));
 
