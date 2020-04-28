993a6f12a1666adf9175ed1a3891c7c324eeaa0d
Julie T <joules2319@gmail.com>
2020-04-28 17:31:09 -0600

    added battle div to battle page

--- client/src/components/pages/BattlePage.js
+++ client/src/components/pages/BattlePage.js
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import Navbar from "../layout/Navbar";
+import Battle from "../battlediv/battle";
 class BattlePage extends Component {
   render() {
     return (
       <div>
         <Navbar />
-        <div className="container valign-wrapper">
-          <div className="row">
-            <div className="col s12 battle">
+        <div className="section">
 
-            </div>
-          </div>
-          <div className="row">
-            <div className="col s12">
+          <div className="container">
+            <Battle />
+            <div className="row">
+              <div className="col s12">
 
+              </div>
             </div>
           </div>
         </div>
