9e40d9219a0e6659a3ba41e9d0d8ed95cc8bb497
tylerphopho <tyler.phopho@gmail.com>
2020-04-29 20:30:38 -0600

    Started making User and Boss cards

--- client/src/components/cards/UserCard.js
+++ client/src/components/cards/UserCard.js
@@ -4,22 +4,31 @@ import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css";
 
 export default class  UserCard extends Component {
-    render() {
-        return(
-            <div>
-                <Navbar/>
-                <div className="container">
-                  <div className="row">
-                    <div className="col s6">
-                      <ul className="collection with-header avatar center"><h4>User Information Header</h4>
-                      </ul>
-                    </div>
-                    <div className="col s6">
-                      <ul className="collection with-header center"><h4>Boss Header</h4></ul>
-                    </div>
-                  </div>
-                </div>
-            </div>
-        );
+  componentDidMount(){
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.collapsible');
+        var instances = M.Collapsible.init(elems, {});
+      });
     }
+  render() {
+    return(
+      <div>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col s6">
+            <div className="collection with-header center"><h3>User Information Header</h3></div>
+            <ul className="collapsible">
+              <li className="collapsible-header"><h4>Character 1</h4></li>
+              <li className="collapsible-body"><span>Stats</span></li>
+            </ul>
+            </div>
+            <div className="col s6">
+            <div className="collection with-header center"><h4>Boss Header</h4></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
