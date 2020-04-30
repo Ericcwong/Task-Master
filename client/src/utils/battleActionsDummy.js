2c35d4f6bbedff87671b3edd0e143061702fac44
Julie T <joules2319@gmail.com>
2020-04-29 21:25:24 -0600

    added dummy actions

---
+++ client/src/utils/battleActionsDummy.js
@@ -0,0 +1,18 @@
+export var battleActions = [
+  {
+    action: "Fight",
+    classes: "btn red margin1"
+  },
+  {
+    action: "Heal",
+    classes: "btn green margin1"
+  },
+  {
+    action: "Spell",
+    classes: "btn blue margin1"
+  },
+  {
+    action: "Rest",
+    classes: "btn yellow darken-4 margin1"
+  }
+]
