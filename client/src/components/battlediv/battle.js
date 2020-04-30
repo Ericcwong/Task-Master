91f3c79baf676ad8c8128224c3c6fea1f539f2dc
Julie T <joules2319@gmail.com>
2020-04-29 22:14:34 -0600

    created stats bar player element

--- client/src/components/battlediv/battle.js
+++ client/src/components/battlediv/battle.js
@@ -4,6 +4,7 @@ import "./battle.css";
 import Boss from "./boss-image.png";
 import { battleActions } from "../../utils/battleActionsDummy";
 import Battleaction from "../battleactions/battleaction";
+import Player from "../playerinfo/playerinfo";
 class Battle extends Component {
   render() {
     return (
@@ -12,10 +13,15 @@ class Battle extends Component {
           <img src={Boss} className="responsive-img" alt="boss sprite" />
         </div>
         <div className="row center-align">
+          <div className="col s10 offset-s1">
+            <Player />
+          </div>
+        </div>
+        <div className="row center-align">
           {
             battleActions.map(action =>
               <Battleaction
-                classes={action.classes}
+                classes={"btn margin1 " + action.color + " black-text"}
                 action={action.action}
               />
             )
