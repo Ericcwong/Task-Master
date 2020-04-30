9c0912c7a1b61618b94a047422e3b3be5dfff324
Julie T <joules2319@gmail.com>
2020-04-29 20:10:10 -0600

    bring battle action button into battlepage

--- client/src/components/battlediv/battle.js
+++ client/src/components/battlediv/battle.js
@@ -2,6 +2,8 @@
 import React, { Component } from "react";
 import "./battle.css";
 import Boss from "./boss-image.png";
+
+import Battleaction from "../battleactions/battleaction";
 class Battle extends Component {
   render() {
     return (
@@ -9,6 +11,11 @@ class Battle extends Component {
         <div className="col s12 center-align">
           <img src={Boss} className="img-fluid" alt="boss sprite" />
         </div>
+        <div className="row center-align">
+          <Battleaction />
+        </div>
+
+
       </div>
     );
   }
