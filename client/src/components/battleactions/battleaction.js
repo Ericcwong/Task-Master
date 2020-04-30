7acfb59d7f09368da73790de2bae1d43dda9e538
Julie T <joules2319@gmail.com>
2020-04-29 21:26:25 -0600

    adjusted action buttons to singular element

--- client/src/components/battleactions/battleaction.js
+++ client/src/components/battleactions/battleaction.js
@@ -3,10 +3,7 @@ import React, { Component } from "react";
 class Battleaction extends Component {
   render() {
     return (
-      <div className="btn center-align rounded">
-
-        <p>{this.props.action}</p>
-      </div>
+      <p className={this.props.classes}>{this.props.action}</p>
     );
   }
 }
