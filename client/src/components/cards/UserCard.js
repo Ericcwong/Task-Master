49deee41fb9030217a19563adb60534f1b761005
Julie T <joules2319@gmail.com>
2020-05-04 21:17:50 -0600

    not quite working dashboard

--- client/src/components/cards/UserCard.js
+++ client/src/components/cards/UserCard.js
@@ -1,40 +1,25 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Navbar from "../layout/Navbar";
 import CreateTodo from "../tasks/CreateTodo"
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css";
 
-export default class  UserCard extends Component {
-  componentDidMount(){
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.collapsible');
-        var instances = M.Collapsible.init(elems, {});
-      });
-    }
+export default class UserCard extends Component {
+  componentDidMount() {
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.collapsible');
+      var instances = M.Collapsible.init(elems, {});
+    });
+  }
   render() {
-    return(
+    return (
       <div>
-        <Navbar />
-        <div className="container">
-          <div className="row">
-            <div className="col s6">
-            <div className="collection with-header center blue darken-4">
-              <h3 className="white-text">User Information Header</h3>
-            </div>
-            <ul className="collapsible">
-              <li>
-              <div className="collapsible-header"><h4>Character 1</h4></div>
-              <div className="collapsible-body"><span><CreateTodo/></span></div>
-              </li>
-            </ul>
-            </div>
-            <div className="col s6">
-            <div className="collection with-header center blue darken-4">
-              <h3 className="white-text">Boss Header</h3>
-            </div>
-            </div>
-          </div>
-        </div>
+        <ul className="collapsible">
+          <li>
+            <div className="collapsible-header"><h4>{this.props.name}</h4></div>
+            <div className="collapsible-body"><span><CreateTodo /></span></div>
+          </li>
+        </ul>
       </div>
     );
   }
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,72 +5,53 @@ import { logoutUser } from "../../actions/authActions";
 import Navbar from "../layout/Navbar";
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css";
+import axios from "axios";
+import UserCard from "../cards/UserCard"
+// import { character } from "../../utils/battleDummyCharacter";
 
 class Dashboard extends Component {
-  componentDidMount(){
-    document.addEventListener('DOMContentLoaded', function() {
-        var elems = document.querySelectorAll('.collapsible');
-        var instances = M.Collapsible.init(elems, {});
-      });
-    }
 
+  componentDidMount() {
+    document.addEventListener('DOMContentLoaded', function () {
+      var elems = document.querySelectorAll('.collapsible');
+      var instances = M.Collapsible.init(elems, {});
+    });
+
+  }
+
+  characters = id => {
+    axios.get("/api/user/" + id + "/characters")
+  }
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
   render() {
+
     const { user } = this.props.auth;
-    return (  
-    <div>
+    let chars = characters(user.id)
+    return (
+      <div>
         <Navbar />
         <div className="container">
           <div className="row">
             <div className="col s6">
-            <div className="collection with-header center blue darken-4">
-              <h3 className="white-text">{user.name.split(" ")[0]}'s Characters</h3>
-            </div>
+              <div className="collection with-header center blue darken-4">
+                <h3 className="white-text">{user.name.split(" ")[0]}'s Characters</h3>
+              </div>
+              {
+                chars.map(character =>
+                  <UserCard
+                    name={character.name}
+                  />)
+              }
 
-            <ul className="collapsible">
-              <li>
-              <div className="collapsible-header"><h4>Character 1</h4></div>
-              <div className="collapsible-body"><span>Stats</span></div>
-              </li>
-            </ul>
-            <ul className="collapsible">
-            <li>
-              <div className="collapsible-header"><h4>Character 2</h4></div>
-              <div className="collapsible-body"><span>Stats</span></div>
-              </li>
-            </ul>
-            <ul className="collapsible">
-            <li>
-              <div className="collapsible-header"><h4>Character 3</h4></div>
-              <div className="collapsible-body"><span>Stats</span></div>
-              </li>
-            </ul>
-            <ul className="collapsible">
-            <li>
-              <div className="collapsible-header"><h4>Character 4</h4></div>
-              <div className="collapsible-body"><span>Stats</span></div>
-              </li>
-            </ul>
-            <ul className="collapsible">
-            <li>
-              <div className="collapsible-header"><h4>Character 5</h4></div>
-              <div className="collapsible-body"><span>Stats</span></div>
-              </li>
-            </ul>
-            </div>
-            <div className="col s6">
-            <div className="collection with-header center blue darken-4   ">
-              <h3 className="white-text">Boss Header</h3>
-            </div>
             </div>
           </div>
         </div>
+
       </div>
-              
-      
+
     );
   }
 }
