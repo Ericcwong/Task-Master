a5357967aeb90d17c4d8cb32bdf1b8bbe5b50683
Dan D <degeare28@gmail.com>
2020-05-04 20:45:35 -0600

    Migrated user card to dashboard

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -2,41 +2,75 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
-// import UserCard from "../UserCard.js"
-// import Navbar from "../layout/Navbar";
+import Navbar from "../layout/Navbar";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css";
+
 class Dashboard extends Component {
+  componentDidMount(){
+    document.addEventListener('DOMContentLoaded', function() {
+        var elems = document.querySelectorAll('.collapsible');
+        var instances = M.Collapsible.init(elems, {});
+      });
+    }
+
   onLogoutClick = e => {
     e.preventDefault();
     this.props.logoutUser();
   };
   render() {
     const { user } = this.props.auth;
-    return (
-      <div style={{ height: "75vh" }} className="container valign-wrapper">
-        <div className="row">
-          <div className="col s12 center-align">
-            <h4>
-              <b>Hey there,</b> {user.name.split(" ")[0]}
-              <p className="flow-text grey-text text-darken-1">
-                Welcome to <span style={{ fontFamily: "monospace" }}>TASK</span>{" "}
-                master 👏
-              </p>
-            </h4>
-            <button
-              style={{
-                width: "150px",
-                borderRadius: "3px",
-                letterSpacing: "1.5px",
-                marginTop: "1rem"
-              }}
-              onClick={this.onLogoutClick}
-              className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-            >
-              Logout
-            </button>
+    return (  
+    <div>
+        <Navbar />
+        <div className="container">
+          <div className="row">
+            <div className="col s6">
+            <div className="collection with-header center blue darken-4">
+              <h3 className="white-text">{user.name.split(" ")[0]}'s Characters</h3>
+            </div>
+
+            <ul className="collapsible">
+              <li>
+              <div className="collapsible-header"><h4>Character 1</h4></div>
+              <div className="collapsible-body"><span>Stats</span></div>
+              </li>
+            </ul>
+            <ul className="collapsible">
+            <li>
+              <div className="collapsible-header"><h4>Character 2</h4></div>
+              <div className="collapsible-body"><span>Stats</span></div>
+              </li>
+            </ul>
+            <ul className="collapsible">
+            <li>
+              <div className="collapsible-header"><h4>Character 3</h4></div>
+              <div className="collapsible-body"><span>Stats</span></div>
+              </li>
+            </ul>
+            <ul className="collapsible">
+            <li>
+              <div className="collapsible-header"><h4>Character 4</h4></div>
+              <div className="collapsible-body"><span>Stats</span></div>
+              </li>
+            </ul>
+            <ul className="collapsible">
+            <li>
+              <div className="collapsible-header"><h4>Character 5</h4></div>
+              <div className="collapsible-body"><span>Stats</span></div>
+              </li>
+            </ul>
+            </div>
+            <div className="col s6">
+            <div className="collection with-header center blue darken-4   ">
+              <h3 className="white-text">Boss Header</h3>
+            </div>
+            </div>
           </div>
         </div>
       </div>
+              
+      
     );
   }
 }
