60f6db48562f87a1f061442b0aafc34fcdae8cc1
Julie T <joules2319@gmail.com>
2020-05-06 18:36:22 -0600

    altered player component to take in props

--- client/src/components/playerinfo/playerinfo.js
+++ client/src/components/playerinfo/playerinfo.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import Statbar from "../statsbar/statsbar"
-import { character } from "../../utils/battleDummyCharacter";
 class Player extends Component {
   render() {
     return (
       <div>
-        <img src={character.avatar} className="responsive-img max-100px" alt={character.name + " sprite"} />
-        <h3>{character.name}</h3>
+        <img src={this.props.character.avatar} className="responsive-img max-100px" alt={this.props.character.name + " sprite"} />
+        <h3>{this.props.character.name}</h3>
         {
-          character.actions.map(stat =>
+          this.props.character.actions.map(stat =>
             <Statbar
               color={stat.color}
               statValue={stat.statValue}
