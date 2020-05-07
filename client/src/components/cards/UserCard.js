c26af52616a5b63266cfa6e84c601007b75c7402
Julie T <joules2319@gmail.com>
2020-05-06 18:19:23 -0600

    matching to current

--- client/src/components/cards/UserCard.js
+++ client/src/components/cards/UserCard.js
@@ -14,26 +14,16 @@ export default class UserCard extends Component {
   render() {
     return (
       <div>
-<<<<<<< HEAD
-        <ul className="collapsible">
-          <li>
-            <div className="collapsible-header"><h4>{this.props.name}</h4></div>
-            <div className="collapsible-body"><span><CreateTodo /></span></div>
-          </li>
-        </ul>
-      </div>
-=======
         <Navbar />
         <div className="container">
-            <ul className="collapsible">
-              <li>
+          <ul className="collapsible">
+            <li>
               <div className="collapsible-header"><h4>Character 1</h4></div>
-              <div className="collapsible-body"><span><CreateTodo/></span></div>
-              </li>
-            </ul>
-            </div>
-          </div>
->>>>>>> 5a86f56cd7cd827dabaa40db9b2038d72f1a0f77
+              <div className="collapsible-body"><span><CreateTodo /></span></div>
+            </li>
+          </ul>
+        </div>
+      </div>
     );
   }
 }
