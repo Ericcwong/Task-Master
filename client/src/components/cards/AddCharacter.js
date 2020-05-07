5f455ad8f7a1e7f388aaf5390310d5d9eb6a7aa7
Eric Wong <ericwong12@live.com>
2020-05-07 13:31:31 -0600

    Cleaned up the files

--- client/src/components/cards/AddCharacter.js
+++ client/src/components/cards/AddCharacter.js
@@ -1,78 +1,72 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import axios from "axios";
-import "./style.css"
+import "./style.css";
 import "materialize-css/dist/css/materialize.min.css";
-import Collapsible from 'react-collapsible';
+import Collapsible from "react-collapsible";
 export default class AddCharacter extends Component {
-    // Setting the component's initial state
-    constructor(props){
-        super(props)
-        this.state = {
-            name: "",
-            avatar: "",
-            classes: "",
-            healthStat: 200,
-            manaStat: 50,
-            attackStat: 25 
-          };
-    }
-    // componentDidMount(){
-    //     document.addEventListener('DOMContentLoaded', function() {
-    //         var elems = document.querySelectorAll('.collapsible');
-    //         M.Collapsible.init(elems, {});
-    //       });
-    // }
-    onChange = event => {
-        this.setState({ [event.target.id]: event.target.value });
-      };
-    handleFormSubmit = (event, props) =>{
-        event.preventDefault()
-        const newCharacter = {
-            name: this.state.name,
-            avatar: this.state.avatar,
-            classes: this.state.classes,
-            healthStat: this.state.healthStat,
-            manaStat: this.state.manaStat,
-            attackStat: this.state.attackStat
-
-          };
-        console.log(newCharacter);
-        axios
-            .post("http://localhost:3001/api/user/"+this.props.userId+"/characters", newCharacter)
-    }
-    render(){
-        return (
-                <Collapsible 
-                    trigger="Add Chararacter" 
-                    triggerClassName="collection with-header center blue darken-4" 
-                    openedClassName="white"
-                >
-                <form className="form">
-                    <input
-                        value={this.state.name}
-                        id="name"
-                        onChange={this.onChange}
-                        type="text"
-                        placeholder="Character Name"
-                    />
-                    <input
-                        value={this.state.avatar}
-                        id="avatar"
-                        onChange={this.onChange}
-                        type="text"
-                        placeholder="Avatar"
-                    />
-                    <input
-                        value={this.state.classes}
-                        id="classes"
-                        onChange={this.onChange}
-                        type="text"
-                        placeholder="Class"
-                    />
-                    <button onClick={this.handleFormSubmit}>Submit</button>
-                </form>
-                </Collapsible>
-        );
-    }
-};
-
+  // Setting the component's initial state
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      avatar: "",
+      classes: "",
+      healthStat: 200,
+      manaStat: 50,
+      attackStat: 25,
+    };
+  }
+  onChange = (event) => {
+    this.setState({ [event.target.id]: event.target.value });
+  };
+  handleFormSubmit = (event, props) => {
+    event.preventDefault();
+    const newCharacter = {
+      name: this.state.name,
+      avatar: this.state.avatar,
+      classes: this.state.classes,
+      healthStat: this.state.healthStat,
+      manaStat: this.state.manaStat,
+      attackStat: this.state.attackStat,
+    };
+    console.log(newCharacter);
+    axios.post(
+      "http://localhost:3001/api/user/" + this.props.userId + "/characters",
+      newCharacter
+    );
+  };
+  render() {
+    return (
+      <Collapsible
+        trigger="Add Chararacter"
+        triggerClassName="collection with-header center blue darken-4"
+        openedClassName="white"
+      >
+        <form className="form">
+          <input
+            value={this.state.name}
+            id="name"
+            onChange={this.onChange}
+            type="text"
+            placeholder="Character Name"
+          />
+          <input
+            value={this.state.avatar}
+            id="avatar"
+            onChange={this.onChange}
+            type="text"
+            placeholder="Avatar"
+          />
+          <input
+            value={this.state.classes}
+            id="classes"
+            onChange={this.onChange}
+            type="text"
+            placeholder="Class"
+          />
+          <button onClick={this.handleFormSubmit}>Submit</button>
+        </form>
+      </Collapsible>
+    );
+  }
+}
--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -4,73 +4,63 @@ import { connect } from "react-redux";
 import { logoutUser } from "../../actions/authActions";
 import Navbar from "../layout/Navbar";
 import "materialize-css/dist/css/materialize.min.css";
-// import M from "materialize-css";
-// import axios from "axios";
-import AddCharacter from "../cards/AddCharacter"
-import UserCard from "../cards/UserCard"
-
+import AddCharacter from "../cards/AddCharacter";
+import UserCard from "../cards/UserCard";
 class Dashboard extends Component {
-  
   state = {
     loading: true,
-    character: null
+    character: null,
   };
   async componentDidMount() {
     //Pulls user's id and gets the url from the backend
     const { user } = this.props.auth;
-    let userID = user.id
-    const getURL = "/api/user/"+userID+"/characters";
+    let userID = user.id;
+    const getURL = "/api/user/" + userID + "/characters";
     //Gets characters from backend
     const response = await fetch(getURL);
-    const data = await response.json()
-    this.setState({character: data, loading: false})
+    const data = await response.json();
+    this.setState({ character: data, loading: false });
     console.log(data);
-    //Adds character to user
-    // export const 
-
   }
-  // export const addCharacter =() => 
-  onLogoutClick = e => {
+  onLogoutClick = (e) => {
     e.preventDefault();
     this.props.logoutUser();
   };
   render() {
-
     const { user } = this.props.auth;
-
     return (
       <div>
         <Navbar />
         <div className="container">
-            <AddCharacter
-              userId={user.id}
-            />
+          <AddCharacter userId={user.id} />
           <div className="row">
-          
             <div className="col s6">
               <div className="collection with-header center blue darken-4">
-                <h3 className="white-text">{user.name.split(" ")[0]}'s Characters</h3>
+                <h3 className="white-text">
+                  {user.name.split(" ")[0]}'s Characters
+                </h3>
               </div>
               {/* If no character is able to be pulled in, says loading till character is able to be pulled */}
-              {this.state.loading || !this.state.character ? <div>Loading... Please Wait</div> : 
+              {this.state.loading || !this.state.character ? (
+                <div>Loading... Please Wait</div>
+              ) : (
                 <div>
-                  <UserCard
-                    CardData={this.state.character}
-                  />
-              </div>}
+                  <UserCard CardData={this.state.character} />
+                </div>
+              )}
             </div>
           </div>
-        </div>​
+        </div>
+        ​
       </div>
-
     );
   }
 }
 Dashboard.propTypes = {
   logoutUser: PropTypes.func.isRequired,
-  auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
-const mapStateToProps = state => ({
-  auth: state.auth
+const mapStateToProps = (state) => ({
+  auth: state.auth,
 });
 export default connect(mapStateToProps, { logoutUser })(Dashboard);
