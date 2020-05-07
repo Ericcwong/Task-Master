8338af68346e2f8063d9b0e4e8b4878a8f00c5d0
Julie T <joules2319@gmail.com>
2020-05-06 18:47:45 -0600

    passing character info as props

--- client/src/components/battlediv/battle.js
+++ client/src/components/battlediv/battle.js
@@ -5,18 +5,25 @@ import Boss from "./boss-image.png";
 import { character } from "../../utils/battleDummyCharacter";
 import Battleaction from "../battleactions/battleaction";
 import Player from "../playerinfo/playerinfo";
+import { boss } from "../../utils/battleDummyBoss"
 
 let battleActions = character.actions
+let playerChar = character;
+let bossChar = boss;
 class Battle extends Component {
   render() {
     return (
       <div className="row battle rounded">
         <div className="col s12 center-align">
-          <img src={Boss} className="responsive-img" alt="boss sprite" />
+          <Player
+            character={bossChar}
+          />
         </div>
         <div className="row center-align">
           <div className="col s10 offset-s1">
-            <Player />
+            <Player
+              character={playerChar}
+            />
           </div>
         </div>
         <div className="row center-align">
