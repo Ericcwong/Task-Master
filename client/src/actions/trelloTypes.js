9e8c7ce939984adb1181afa0ccc27f3700cebe4e
Dalto <dalton.earl@outlook.com>
2020-05-08 15:36:52 -0600

    added action types in trelloTypes.js

---
+++ client/src/actions/trelloTypes.js
@@ -0,0 +1,5 @@
+export const GET_BOARDS = "GET_BOARDS";
+export const GET_BOARD = "GET_BOARD";
+export const GET_CARDS = "GET_CARDS";
+export const GET_CARD = "GET_CARD";
+export const GET_BOARD_MEMBERS = "GET_BOARD_MEMBERS";
