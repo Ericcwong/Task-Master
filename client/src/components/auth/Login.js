f67cafe0790d116c5684d4d9e97a248819195a5a
Julie T <joules2319@gmail.com>
2020-05-09 00:13:29 -0600

    reverted login prop type

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -119,7 +119,7 @@ class Login extends Component {
 Login.propTypes = {
   loginUser: PropTypes.func.isRequired,
   auth: PropTypes.object.isRequired,
-  errors: PropTypes.string.isRequired
+  errors: PropTypes.object.isRequired
 };
 const mapStateToProps = state => ({
   auth: state.auth,
