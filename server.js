a36c55a67b8ebe76e391c5ff7396943e51e051ad
Julie T <joules2319@gmail.com>
2020-05-09 02:32:40 -0600

    temporraily removed trello req from server

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const cors = require("cors");
 // File calls
 const users = require("./routes/api/users");
 const routes = require("./routes");
-const trello = require("./routes/api/trello");
+// const trello = require("./routes/api/trello");
 const app = express();
 var OAuth = require('oauth').OAuth
 const PORT = process.env.PORT || 3001;
