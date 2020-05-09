05959ce30c471885843daff3bb4c091ead895b9e
Eric Wong <ericwong12@live.com>
2020-05-08 21:10:01 -0600

    added another localhost

--- client/src/components/cards/UserCard.js
+++ client/src/components/cards/UserCard.js
@@ -12,7 +12,7 @@ export default class UserCard extends Component {
     this.getCharacter();
   }
   getCharacter = async () =>{
-   let data = await Axios.get("/api/user/"+this.props.CardData.user+"/character")
+   let data = await Axios.get("http://localhost:3001/api/user/"+this.props.CardData.user+"/character")
     .then(({data}) => data);
     // this.setState({character: data})
   }
