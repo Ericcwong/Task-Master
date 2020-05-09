e197b8a9cf83e03db6c170ef45d18890d6dd89c8
DanDukes <degeare28@gmail.com>
2020-05-09 08:30:37 -0600

    more spelling

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 module.exports = {
     User: require("./user"),
-    Task: require("./Task"),
-    Project: require("./Project"),
+    Task: require("./task"),
+    Project: require("./project"),
     Character: require("./character"),
     Team: require("./team")
   };
