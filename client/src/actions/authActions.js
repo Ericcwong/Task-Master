72e951f98f39274842174d52dda51935e623ac70
Julie T <joules2319@gmail.com>
2020-05-08 23:33:48 -0600

    reverted get to post

--- client/src/actions/authActions.js
+++ client/src/actions/authActions.js
@@ -17,7 +17,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .get("/api/users/login", userData)
+    .post("/api/users/login", userData)
     .then(res => {
       // Save to localStorage
       // Set token to localStorage
