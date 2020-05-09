e98aa21c1b1fb6191091faaf8871c60cda4cb6c1
DanDukes <degeare28@gmail.com>
2020-05-09 08:12:00 -0600

    changed model call

--- routes/api/users.js
+++ routes/api/users.js
@@ -9,7 +9,7 @@ const validateRegisterInput = require("../../validation/register");
 const validateLoginInput = require("../../validation/login");
 
 // Load User model
-const User = require("../../models/User");
+const User = require("../../models");
 
 // @route POST api/users/register
 // @desc Register user
