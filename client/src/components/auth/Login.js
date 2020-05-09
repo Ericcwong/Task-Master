f411d8c79f3f0b20bc13879c4de80c00e6b32afb
Julie T <joules2319@gmail.com>
2020-05-08 22:57:00 -0600

    added this.props.history

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -40,7 +40,7 @@ class Login extends Component {
       email: this.state.email,
       password: this.state.password
     };
-    this.props.loginUser(userData); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
+    this.props.loginUser(userData, this.props.history); // since we handle the redirect within our component, we don't need to pass in this.props.history as a parameter
   };
   render() {
     const { errors } = this.state;
